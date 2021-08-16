24a3286e634a78ed5c12305b02613d1922743a6a
Paulina Koczwara <paulinka.koczwara@gmail.com>
2021-08-16 19:38:20 +0930

    added mongo link

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import { QUERY_USER } from '../api/queries';
 import { useQuery } from '@apollo/client';
@@ -12,9 +13,9 @@ const Profile = () => {
   return (
     <div>
       <h1>Profile</h1>
-      <p>First Name: {data.user.firstName}</p>
-      <p>Last Name: {data.user.lastName}</p>
-      <p>Email: {data.user.email}</p>
+      <p className="p-3 border rounded block w-full mb-3 text-center border-purple-200 text-pink-500">First Name: {data.user.firstName}</p>
+      <p className="p-3 border rounded block w-full mb-3 text-center border-purple-200 text-pink-500">Last Name: {data.user.lastName}</p>
+      <p className="p-3 border rounded block w-full mb-3 text-center border-purple-200 text-pink-500">Email: {data.user.email}</p>
     </div>
   );
 };
